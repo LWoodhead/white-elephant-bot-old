441579cb5068459d9e4df7ed9f6801762c19c4dc
Luke Woodhead <lukejwoodhead@gmail.com>
2024-03-05 15:17:33 -0700

    Fixed type error

--- discord_bot/classes/actionRecord.py
+++ discord_bot/classes/actionRecord.py
@@ -3,6 +3,6 @@ class ActionRecord:
     #data should be a dict with all relevant state information to undo the action
     #the type field should be the type of action: steal, pass, unwrap, new round, game end, etc 
     def __init__(self, id: int, 
-                 data: {"type" : str, 'info': int, "..." : str}) -> None:
+                 data: {str, int, str}) -> None:
         self.id = id
         self.data = data
