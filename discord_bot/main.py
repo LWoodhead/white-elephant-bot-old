caa55b9620ef447831295d226854b091b5a067c0
Luke Woodhead <lukejwoodhead@gmail.com>
2024-03-25 10:35:21 -0600

    Swaped to using pycord library to interact with discord API

--- discord_bot/main.py
+++ discord_bot/main.py
@@ -1,30 +1,30 @@
 import discord
 import os
 
-intents = discord.Intents.default()
-intents.message_content = True
+bot = discord.Bot()
 
-client = discord.Client(intents=intents)
-
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as {bot.user}')
+    
+@bot.slash_command()
+async def create_game(ctx):
+    # TODO create a unique id. create a gamewrapper object
+    await ctx.respond("Game Created")
+    
+@bot.slash_command()
+async def add_player(ctx):
+    await ctx.respond("Player Added")
+    
+@bot.slash_command()
+async def ping(ctx: discord.ApplicationContext):
+    await ctx.respond("Pong")
+    
+@bot.slash_command()
+async def whip(ctx):
+    await ctx.respond("Nay Nay")      
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-        
-    if message.content.startswith('$Luke'):
-        await message.channel.send('Age 5')
-        
-    if message.content.startswith('$whip'):
-        await message.channel.send('Nay Nay')        
-
-if os.environ.get('DISCORD_TOKEN') != None:
-    client.run(os.environ.get('DISCORD_TOKEN'))
+if os.environ.get('DISCORD_TOKEN') is not None:
+    bot.run(os.environ.get('DISCORD_TOKEN'))
 else:
     print("Exiting no valid token")
